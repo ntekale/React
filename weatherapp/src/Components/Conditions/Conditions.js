74416d46b1db887de3117ad2ed1904dff6273f66
ntekale <63825114+ntekale@users.noreply.github.com>
2021-09-18 14:42:48 +1000

    Add files via upload

---
+++ weatherapp/src/Components/Conditions/Conditions.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './Conditions.module.css';
+
+const Conditions = (props) => {
+   return (
+       <div className={classes.Wrapper}>
+           {props.responseObj.cod === 200 ?
+               <div>
+                   <p><strong>{props.responseObj.name}</strong></p>
+                   <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
+               </div>
+           : null
+           }
+       </div>
+   )
+}
+
+export default Conditions;
